2c6030b3139bb2db31a85835bcd6d0b48930c298
pavithragoud <sudhakargoud384@gmail.com>
2025-09-10 16:36:43 +0530

    Add files via upload

---
+++ Nonprimitive.java
@@ -0,0 +1,39 @@
+package simple;
+
+
+
+public class Nonprimitive {
+
+
+
+	public static void main(String[] args) {
+
+		String std="pavithra goud";
+
+		String[] map= {"1st year =fundamentals",
+
+				"2nd year=learn ai improve skills",
+
+				"3rd year=interships",
+
+				"4th year=placements"
+
+		};
+
+		System.out.println("career road map");
+
+		System.out.println("student name="+std);
+
+		for(String step:map) {
+
+			System.out.println("->"+step);
+
+
+
+		}
+
+
+
+	}
+
+}
